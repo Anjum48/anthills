2ec420bd9a6eacb82b2b0630f5ad58a58ca56017
Anjum Sayed <anjum.sayed48@gmail.com>
2017-05-02 12:29:26 +0100

    Experimenting using T2 cutoffs in permeability calculation

--- permeability_calculation.py
+++ permeability_calculation.py
@@ -1,23 +1,32 @@
 import os
 import pandas as pd
+import numpy as np
 import lasio
 
 well = "R-538"
 las_file = os.path.join(os.pardir, "Input_files", well+"_CMR.las")
 output_folder = os.pardir
-output_folder_name = "NMRGP_Output_Cored"
+output_folder_name = "anthills_output"
 
 if not os.path.exists(os.path.join(output_folder, output_folder_name, "Permeability")):
     os.makedirs(os.path.join(output_folder, output_folder_name, "Permeability"))
 
-means = pd.read_csv(os.path.join(os.pardir, "NMRGP_Output_Cored", "csv", well+"_means.csv"), index_col=0)
-weights = pd.read_csv(os.path.join(os.pardir, "NMRGP_Output_Cored", "csv", well+"_weights.csv"), index_col=0)
+means = pd.read_csv(os.path.join(os.pardir, output_folder_name, "CSV", well+"_means.csv"), index_col=0)
+weights = pd.read_csv(os.path.join(os.pardir, output_folder_name, "CSV", well+"_weights.csv"), index_col=0)
 phi_nmr = lasio.read(las_file).df().dropna()["TCMR"]
 phi_nmr = phi_nmr.values.reshape((len(phi_nmr), 1))
 
 if len(means) == len(weights) == len(phi_nmr):
-    perm = (((phi_nmr*weights)**4) * ((10**means)**2)).sum(axis=1)
-    perm.rename("NMRGP_Perm", inplace=True)
-    perm.to_csv(os.path.join(output_folder, output_folder_name, "Permeability", well+"_NMRGP.csv"), header=True)
+    perm_dict = {}
+    for cutoff in [0.3, 50, 100]:
+        means_filtered = means.where(means > np.log10(cutoff)).fillna(0)
+        weights_filtered = weights.where(means > np.log10(cutoff))
+        weights_filtered = (weights_filtered / weights_filtered.sum(axis=0)).fillna(0)
+
+        perm_dict["Perm_GMM_"+str(cutoff)+"ms"] = (((phi_nmr*weights)**4) * ((10**means)**2)).sum(axis=1)
+        # perm.rename("Perm_GMM", inplace=True)
+        # perm.to_csv(os.path.join(output_folder, output_folder_name, "Permeability", well+"_NMRGMM.csv"), header=True)
+    pd.DataFrame(perm_dict).to_csv(os.path.join(output_folder, output_folder_name, "Permeability", well+"_NMRGMM.csv"))
+
 else:
     print("Mismatch between the number of PHIT samples and processed Gaussians:", len(phi_nmr), "vs", len(weights))
